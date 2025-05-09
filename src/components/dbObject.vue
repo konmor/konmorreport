be85e84e34d8e32e81d7dd34319336d31b24d44a
konmor <1975492517@qq.com>
2025-05-09 16:14:39 +0800

    数据库对象单独使用一个组件

--- src/components/dbObject.vue
+++ src/components/dbObject.vue
@@ -5,13 +5,216 @@ export default {
 </script>
 <script setup lang="ts">
 
+import {
+  FieldNumberOutlined,
+  FieldStringOutlined,
+  FieldTimeOutlined,
+  FundViewOutlined,
+  TableOutlined
+} from "@ant-design/icons-vue";
+import {reactive, ref, watch} from "vue";
+import type {TreeProps} from "ant-design-vue";
+
+type DataType = TreeProps & {
+  dataType?: 'number' | 'string' | 'date'
+  type?: 'table' | 'view'
+}
+
+let siderHeight = ref(800)
+const treeData: DataType['treeData'] = reactive([
+  {
+    title: '表格',
+    key: 'table',
+    type: 'table',
+
+    children: [
+      {
+        title: 'worldfdasfdsafadsfsadfdsa',
+        key: 'world',
+        children: [
+          {
+            title: 'worldCode',
+            key: 'worldCode',
+            dataType: 'number',
+          },
+          {
+            title: 'worldName',
+            key: 'worldName',
+            dataType: 'string',
+          },
+          {
+            title: 'peopleCount',
+            key: 'peopleCount',
+            dataType: 'number',
+          },
+          {
+            title: 'createDatefdsdfsfdasfdsaf',
+            key: 'createDate',
+            dataType: 'date',
+          },
+        ],
+      },
+      {
+        title: 'user',
+        key: 'user',
+      },
+    ],
+  },
+  {
+    title: '视图',
+    key: 'view',
+    type: 'view',
+
+    children: [
+      {
+        title: 'parent 1-0',
+        key: '0-0-0',
+      },
+      {
+        title: 'parent 1-0',
+        key: '0-0-0',
+      },
+      {
+        title: 'parent 1-0',
+        key: '0-0-0',
+      },
+      {
+        title: 'parent 1-0',
+        key: '0-0-0',
+      },
+      {
+        title: 'parent 1-0',
+        key: '0-0-0',
+      },
+      {
+        title: 'parent 1-0',
+        key: '0-0-0',
+      },
+      {
+        title: 'parent 1-0',
+        key: '0-0-0',
+      },
+      {
+        title: 'parent 1-0',
+        key: '0-0-0',
+      },
+      {
+        title: 'parent 1-0',
+        key: '0-0-0',
+      },
+      {
+        title: 'parent 1-0',
+        key: '0-0-0',
+      },
+      {
+        title: 'parent 1-0',
+        key: '0-0-0',
+      },
+      {
+        title: 'parent 1-0',
+        key: '0-0-0',
+      },
+      {
+        title: 'parent 1-0',
+        key: '0-0-0',
+      },
+      {
+        title: 'parent 1-0',
+        key: '0-0-0',
+      },
+      {
+        title: 'parent 1-0',
+        key: '0-0-0',
+      },
+      {
+        title: 'parent 1-0',
+        key: '0-0-0',
+      },
+      {
+        title: 'parent 1-0',
+        key: '0-0-0',
+      },
+      {
+        title: 'parent 1-0',
+        key: '0-0-0',
+      },
+      {
+        title: 'parent 1-0',
+        key: '0-0-0',
+      },
+      {
+        title: 'parent 1-0',
+        key: '0-0-0',
+      },
+      {
+        title: 'parent 1-0',
+        key: '0-0-0',
+      },
+      {
+        title: 'parent 1-1',
+        key: '0-0-1',
+      },
+    ],
+  },
+]);
+const expandedKeys = ref<string[]>(['view', 'table'])
+watch(expandedKeys, () => {
+  console.log('expandedKeys', expandedKeys)
+})
 </script>
 
 
 <template>
+  <a-layout-sider theme="light" :style="{marginRight:'10px'}">
+    <a-input-search v-model:value="searchValue" style="margin-bottom: 8px" placeholder="Search" />
 
+    <div
+        :style="{
+          height: `${siderHeight}+ px`,
+        }"
+    >
+      <a-tree
+          :tree-data="treeData"
+          v-model:expandedKeys="expandedKeys"
+          :style="{ width: '100%' }"
+          :height="siderHeight - 1"
+      >
+        <template #title="{ title, key, type, dataType }">
+          <a-tooltip :title="title">
+            <span v-if="type === 'view'">
+              <fund-view-outlined></fund-view-outlined>
+              {{ title }}
+            </span>
+          <span v-else-if="type === 'table'">
+              <table-outlined></table-outlined>
+              {{ title }}
+          </span>
+          <span v-else-if="dataType === 'string'" class="fields">
+              <field-string-outlined style="color: #efb056"></field-string-outlined>
+              {{ title }}</span>
+          <span v-else-if="dataType === 'number'" class="fields">
+              <field-number-outlined style="color: #6fd845"></field-number-outlined>
+              {{ title }}</span>
+          <span v-else-if="dataType === 'date'" class="fields">
+              <field-time-outlined style="color: #1890ff"></field-time-outlined>
+              {{ title }}</span>
+
+          <template v-else>
+            <span class="fields">{{ title }}</span>
+          </template>
+          </a-tooltip>
+        </template>
+      </a-tree>
+    </div>
+  </a-layout-sider>
 </template>
 
 <style scoped>
-
+.fields {
+  max-width: 110px;
+  white-space: nowrap;
+  overflow: hidden;
+  text-overflow: ellipsis;
+  display: inline-block;
+}
 </style>
--- src/components/sqlEditor.vue
+++ src/components/sqlEditor.vue
@@ -5,14 +5,8 @@ export default {
 </script>
 
 <script setup lang="ts">
-import type { TreeProps } from 'ant-design-vue'
-import { inject, onMounted, reactive, ref, watch } from 'vue'
+import { inject, onMounted, reactive, ref } from 'vue'
 import {
-  FundViewOutlined,
-  FieldTimeOutlined,
-  FieldNumberOutlined,
-  FieldStringOutlined,
-  TableOutlined,
   WindowsOutlined,
 } from '@ant-design/icons-vue'
 
@@ -22,6 +16,7 @@ import { basicSetup } from 'codemirror'
 import { sql } from '@codemirror/lang-sql'
 import { format } from 'sql-formatter'
 import type { Router } from 'vue-router'
+import DbObject from "@/components/dbObject.vue";
 
 let router = inject<Router>('router')
 
@@ -100,163 +95,13 @@ function sqlExplain(event: Event) {
   }
 }
 
-type DataType = TreeProps & {
-  dataType?: 'number' | 'string' | 'date'
-  type?: 'table' | 'view'
-}
-
-const treeData: DataType['treeData'] = reactive([
-  {
-    title: '表格',
-    key: 'table',
-    type: 'table',
-
-    children: [
-      {
-        title: 'worldfdasfdsafadsfsadfdsa',
-        key: 'world',
-        children: [
-          {
-            title: 'worldCode',
-            key: 'worldCode',
-            dataType: 'number',
-          },
-          {
-            title: 'worldName',
-            key: 'worldName',
-            dataType: 'string',
-          },
-          {
-            title: 'peopleCount',
-            key: 'peopleCount',
-            dataType: 'number',
-          },
-          {
-            title: 'createDatefdsdfsfdasfdsaf',
-            key: 'createDate',
-            dataType: 'date',
-          },
-        ],
-      },
-      {
-        title: 'user',
-        key: 'user',
-      },
-    ],
-  },
-  {
-    title: '视图',
-    key: 'view',
-    type: 'view',
-
-    children: [
-      {
-        title: 'parent 1-0',
-        key: '0-0-0',
-      },
-      {
-        title: 'parent 1-0',
-        key: '0-0-0',
-      },
-      {
-        title: 'parent 1-0',
-        key: '0-0-0',
-      },
-      {
-        title: 'parent 1-0',
-        key: '0-0-0',
-      },
-      {
-        title: 'parent 1-0',
-        key: '0-0-0',
-      },
-      {
-        title: 'parent 1-0',
-        key: '0-0-0',
-      },
-      {
-        title: 'parent 1-0',
-        key: '0-0-0',
-      },
-      {
-        title: 'parent 1-0',
-        key: '0-0-0',
-      },
-      {
-        title: 'parent 1-0',
-        key: '0-0-0',
-      },
-      {
-        title: 'parent 1-0',
-        key: '0-0-0',
-      },
-      {
-        title: 'parent 1-0',
-        key: '0-0-0',
-      },
-      {
-        title: 'parent 1-0',
-        key: '0-0-0',
-      },
-      {
-        title: 'parent 1-0',
-        key: '0-0-0',
-      },
-      {
-        title: 'parent 1-0',
-        key: '0-0-0',
-      },
-      {
-        title: 'parent 1-0',
-        key: '0-0-0',
-      },
-      {
-        title: 'parent 1-0',
-        key: '0-0-0',
-      },
-      {
-        title: 'parent 1-0',
-        key: '0-0-0',
-      },
-      {
-        title: 'parent 1-0',
-        key: '0-0-0',
-      },
-      {
-        title: 'parent 1-0',
-        key: '0-0-0',
-      },
-      {
-        title: 'parent 1-0',
-        key: '0-0-0',
-      },
-      {
-        title: 'parent 1-0',
-        key: '0-0-0',
-      },
-      {
-        title: 'parent 1-1',
-        key: '0-0-1',
-      },
-    ],
-  },
-])
-
 function onSelect(selectedKeys: any, info: any) {
   console.log('Selected:', selectedKeys, info)
 }
 
-const expandedKeys = ref<string[]>(['view', 'table'])
-watch(expandedKeys, () => {
-  console.log('expandedKeys', expandedKeys)
-})
-let siderHeight = ref(800)
-
 let topHeight = ref(500)
 let bottomHeight = ref(260)
 let isDragging = ref(false)
-
-// let contentContainer = ref()
 
 function changeHeight(e: Event) {
   let element = document.getElementById('_contentContainer')
@@ -315,53 +160,12 @@ function saveSQLConfig() {
 
 
 }
+
 </script>
 
 <template>
   <a-layout>
-    <a-layout-sider theme="light">
-      <div style="height: 80px; margin: 10px; border: 1px solid black">搜索框占位</div>
-      <div
-        :style="{
-          height: `${siderHeight}+ px`,
-          border: '1px solid #FF2B2D30',
-        }"
-      >
-        <a-tree
-          :tree-data="treeData"
-          v-model:expandedKeys="expandedKeys"
-          :style="{ width: '100%' }"
-          :height="siderHeight - 1"
-        >
-          <template #title="{ title, key, type, dataType }">
-            <span v-if="type === 'view'">
-              <fund-view-outlined></fund-view-outlined>
-              {{ title }}</span
-            >
-            <span v-else-if="type === 'table'">
-              <table-outlined></table-outlined>
-              {{ title }}</span
-            >
-            <span v-else-if="dataType === 'string'" class="fields">
-              <field-string-outlined style="color: #efb056"></field-string-outlined>
-              {{ title }}</span
-            >
-            <span v-else-if="dataType === 'number'" class="fields">
-              <field-number-outlined style="color: #6fd845"></field-number-outlined>
-              {{ title }}</span
-            >
-            <span v-else-if="dataType === 'date'" class="fields">
-              <field-time-outlined style="color: #1890ff"></field-time-outlined>
-              {{ title }}</span
-            >
-
-            <template v-else
-              ><span class="fields">{{ title }}</span>
-            </template>
-          </template>
-        </a-tree>
-      </div>
-    </a-layout-sider>
+    <db-object/>
     <a-layout>
       <a-layout-header :style="{ backgroundColor: '#FFF', border: '1px solid black' }">
         <a-row gutter="24">
@@ -441,19 +245,10 @@ function saveSQLConfig() {
 </template>
 
 <style scoped>
-.fields {
-  max-width: 110px;
-  white-space: nowrap;
-  overflow: hidden;
-  text-overflow: ellipsis;
-  display: inline-block;
-}
-
 .contentContainer {
   display: flex;
   flex-direction: column;
 }
-
 .contentContainer .top {
   background-color: #1890ff;
 }
