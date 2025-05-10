d7c16d4a2a10a7c314568b865e53e27ac2fbde2b
konmor <1975492517@qq.com>
2025-05-10 19:50:50 +0800

    模态框

--- src/components/navigator.vue
+++ src/components/navigator.vue
@@ -15,7 +15,7 @@ import {
   EditOutlined
 } from '@ant-design/icons-vue'
 import {reactive, ref, watch, h, onMounted, inject} from 'vue'
-import type {MenuProps, ItemType} from 'ant-design-vue'
+import {type MenuProps, type ItemType, Modal} from 'ant-design-vue'
 import type {Router} from 'vue-router'
 import useNavigator from '@/hooks/useNavigator.ts'
 import addDatasourceIcon from "@/components/button/addDatasource.vue";
@@ -41,42 +41,81 @@ let sqlShowButton = reactive(new Array<Boolean>(sqlArray.length))
 let newLabel = ref('数据源')
 let newKey = ref('_datasourceKey')
 let newFlag = ref(1)
+let createDatasource = ref(false);
+
+// 展示告警模态框
+/**
+ * 返回true 则跳转到其他页面
+ * 返回false 则继续新增
+ * 可以利用此返回值，当true时继续其他操作，当false时阻断当前操作。
+ */
+function showAlterModal(confirm: Function) {
+  // 只有正在创建数据源才可显示模态框
+  if (createDatasource.value) {
+    Modal.warning({
+      title: '你确认放弃此次新增数据源吗？',
+      content: '点击确认将会放弃此次编辑内容，并跳转到其他页面。取消则返回继续新增数据源',
+      okText: '确认',
+      cancelText: '取消',
+      onOk: () => {
+        createDatasource.value = false;
+        confirm();
+      },
+      onCancel: () => {
+        createDatasource.value = true;
+      }
+    })
+  } else {
+    confirm();
+  }
+  console.log('showAlterModal', createDatasource.value)
+
+}
 
 // 添加数据源
 function addDataSource(event: Event) {
-  event.stopPropagation()
-  let label = newLabel.value + '(' + newFlag.value + ')';
-  let key = newKey.value + newFlag.value;
-  /**
-   * 数据源头的名称
-   * 数据源的类型
-   * ip地址 端口号 账号密码
-   *
-   * 按钮 测试连接 返回连接状态，或者失败信息
-   *
-   * 高级 特性 连接内容 连接池大小 最大连接个数
-   */
-  items.push({
-    label: label,
-    key: key,
-  })
-  selectedKeys.value = [key]
-  newFlag.value += 1
-
-  if (router != null) {
-    router.push({
-      name: 'toDataSourceCreator',
-      query: {
-        key: key,
-        label: label
-      },
+  event.stopPropagation();
+  showAlterModal(() => {
+    let label = newLabel.value + '(' + newFlag.value + ')';
+    let key = newKey.value + newFlag.value;
+    /**
+     * 数据源头的名称
+     * 数据源的类型
+     * ip地址 端口号 账号密码
+     *
+     * 按钮 测试连接 返回连接状态，或者失败信息
+     *
+     * 高级 特性 连接内容 连接池大小 最大连接个数
+     */
+    items.push({
+      label: label,
+      key: key,
     })
-  }
+    selectedKeys.value = [key]
+    newFlag.value += 1
+
+    if (router != null) {
+      router.push({
+        name: 'toDataSourceCreator',
+        query: {
+          key: key,
+          label: label
+        },
+      })
+    }
+    createDatasource.value = true;
+  });
 }
 
 function showDatasourceDetail(item: ItemType, event: Event) {
-  event.stopPropagation()
+  event.stopPropagation();
+  let confirm = showAlterModal();
+  // 如果不通过则阻断下面的操作
+  if (!confirm) {
+    return;
+  }
   if (router != null && item != null) {
+    // 有label值时传递到下级
     if ('label' in item) {
       router.push({
         name: 'toEditDataSourceConfig',
@@ -98,7 +137,12 @@ function showDatasourceDetail(item: ItemType, event: Event) {
 }
 
 function showSQLDetail(key: string, event: Event) {
-  event.stopPropagation()
+  event.stopPropagation();
+  let confirm = showAlterModal();
+  // 如果不通过则阻断下面的操作
+  if (!confirm) {
+    return;
+  }
   if (router != null && key != null) {
     router.push({
       name: 'jumpSqlCreator',
@@ -111,7 +155,13 @@ function showSQLDetail(key: string, event: Event) {
 }
 
 function addSQL(key: string, event: Event) {
+
   event.stopPropagation();
+  let confirm = showAlterModal();
+  // 如果不通过则阻断下面的操作
+  if (!confirm) {
+    return;
+  }
   if (router != null && key != null) {
     router.push({
       name: 'toCreateSQL',
@@ -122,31 +172,48 @@ function addSQL(key: string, event: Event) {
   }
 }
 
-function removeDatasource(key:string,event:Event){
-  event.stopPropagation();
-}
-function checkDatasourceConfig(key:string,event:Event){
-  event.stopPropagation();
-}
-function checkDatasourceData(key:string,event:Event){
-  event.stopPropagation();
-}
-function  removeSQL(key:string,event:Event){
-  event.stopPropagation();
-}
-function  checkSQLConfig(key:string,event:Event){
-  event.stopPropagation();
-}
-function  checkSQLData(key:string,event:Event){
+function removeDatasource(key: string, event: Event) {
   event.stopPropagation();
 }
 
-function a() {
-  console.log('a')
+function checkDatasourceConfig(key: string, event: Event) {
+  event.stopPropagation();
+  let confirm = showAlterModal();
+  // 如果不通过则阻断下面的操作
+  if (!confirm) {
+    return;
+  }
 }
 
-function c() {
-  console.log('c')
+function checkDatasourceData(key: string, event: Event) {
+  event.stopPropagation();
+  let confirm = showAlterModal();
+  // 如果不通过则阻断下面的操作
+  if (!confirm) {
+    return;
+  }
+}
+
+function removeSQL(key: string, event: Event) {
+  event.stopPropagation();
+}
+
+function checkSQLConfig(key: string, event: Event) {
+  event.stopPropagation();
+  let confirm = showAlterModal();
+  // 如果不通过则阻断下面的操作
+  if (!confirm) {
+    return;
+  }
+}
+
+function checkSQLData(key: string, event: Event) {
+  event.stopPropagation();
+  let confirm = showAlterModal();
+  // 如果不通过则阻断下面的操作
+  if (!confirm) {
+    return;
+  }
 }
 
 watch(openKeys, (val) => {
@@ -209,13 +276,16 @@ onMounted(() => {
           @mouseenter="datasourceShowButton[index] = true"
           @mouseleave="datasourceShowButton[index] = false">
         <span v-if="myItem !== null && 'label' in myItem">{{ myItem.label }}</span>
-        <a-button-group v-if="datasourceShowButton[index]"
-                        :style="{display:'inline-flex',justifyContent:'space-between',position:'absolute',right:'8px',top:'8px'}">
+        <a-button-group
+            v-if="datasourceShowButton[index] &&  myItem != null && !String(myItem.key).startsWith('_datasourceKey')"
+            :style="{display:'inline-flex',justifyContent:'space-between',position:'absolute',right:'8px',top:'8px'}">
           <a-tooltip title="创建SQL">
             <a-button
                 size="small"
                 @click="addSQL(myItem?.key as string, $event)">
-              <template #icon><PlusOutlined /></template>
+              <template #icon>
+                <PlusOutlined/>
+              </template>
             </a-button>
           </a-tooltip>
 
