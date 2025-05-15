47a931f2a6519499a9715a8d12e0b3d303bfb7d0
konmor <5363420+konmor@user.noreply.gitee.com>
2025-05-15 09:20:09 +0800

    全局错误处理

--- src/main.ts
+++ src/main.ts
@@ -4,11 +4,7 @@ import router from '@/router'
 import Antd from 'ant-design-vue'
 import 'ant-design-vue/dist/reset.css'
 import { createPinia } from 'pinia'
-import errorHandlers, {
-  type ErrorKeyType,
-  type ErrorMessageType,
-  type ErrorValueType,
-} from '@/utils/errorHandler/HandlerError.ts'
+import errorHandlers, { initErrorHandler } from '@/utils/errorHandler/HandlerError.ts'
 import { ReportsError, ReportsObjectError } from '@/utils/errorHandler/ReportsError.ts'
 
 let app = createApp(App)
@@ -17,44 +13,7 @@ app.use(router)
 app.use(Antd)
 app.use(pinia)
 
-// Vue 全局错误处理器
-
-app.config.errorHandler = (
-  err: unknown,
-  instance: ComponentPublicInstance | null,
-  info: string,
-) => {
-  if (typeof err == 'string') {
-    errorHandlers.handle(err as ErrorKeyType)
-  } else if (err instanceof ReportsObjectError) {
-    errorHandlers.handle(err.errorType as ErrorKeyType, err.errorObj)
-  } else if (err instanceof ReportsError) {
-    errorHandlers.handle(err.name as ErrorKeyType, err.message)
-  } else {
-    errorHandlers.handle('vue')
-  }
-}
-// 原生 JS 错误（语法错误、未定义变量等）
-window.onerror = (message, source, lineno, colno, error) => {
-  errorHandlers.handle('Global-JS-Error')
-  console.error('Global-JS-Error', message, source, lineno, colno, error)
-  return true // 阻止默认行为
-}
-
-// 未捕获的 Promise 拒绝（异步错误）
-window.onunhandledrejection = (event) => {
-  const err = event.reason
-
-  if (err instanceof ReportsObjectError) {
-    errorHandlers.handle(err.errorType as ErrorKeyType, err.errorObj)
-  } else if (err instanceof ReportsError) {
-    errorHandlers.handle(err.name as ErrorKeyType, err.message)
-  } else {
-    errorHandlers.handle('Unhandled-Promise-Rejection')
-  }
-
-  console.error('UnhandledRejection', event)
-  event.preventDefault()
-}
+// 初始化全局错误处理
+initErrorHandler(app);
 
 app.mount('#app')
--- src/utils/errorHandler/HandlerError.ts
+++ src/utils/errorHandler/HandlerError.ts
@@ -1,5 +1,7 @@
 import { message } from 'ant-design-vue'
 import errorHandler from '@/utils/errorHandler/ErrorHandler.ts'
+import type { App, ComponentPublicInstance } from 'vue'
+import { ReportsError, ReportsObjectError } from '@/utils/errorHandler/ReportsError.ts'
 
 export type ErrorMessageType = {
   timeOut: string
@@ -29,3 +31,46 @@ export default errorHandlers
 errorHandlers.bindHandler('save', (value) => {
   message.warning('保存：'+value)
 })
+
+
+export function initErrorHandler(app:App) {
+  // Vue 全局错误处理器
+
+  app.config.errorHandler = (
+    err: unknown,
+    instance: ComponentPublicInstance | null,
+    info: string,
+  ) => {
+    if (typeof err == 'string') {
+      errorHandlers.handle(err as ErrorKeyType)
+    } else if (err instanceof ReportsObjectError) {
+      errorHandlers.handle(err.errorType as ErrorKeyType, err.errorObj)
+    } else if (err instanceof ReportsError) {
+      errorHandlers.handle(err.name as ErrorKeyType, err.message)
+    } else {
+      errorHandlers.handle('vue')
+    }
+  }
+  // 原生 JS 错误（语法错误、未定义变量等）
+  window.onerror = (message, source, lineno, colno, error) => {
+    errorHandlers.handle('Global-JS-Error')
+    console.error('Global-JS-Error', message, source, lineno, colno, error)
+    return true // 阻止默认行为
+  }
+
+  // 未捕获的 Promise 拒绝（异步错误）
+  window.onunhandledrejection = (event) => {
+    const err = event.reason
+
+    if (err instanceof ReportsObjectError) {
+      errorHandlers.handle(err.errorType as ErrorKeyType, err.errorObj)
+    } else if (err instanceof ReportsError) {
+      errorHandlers.handle(err.name as ErrorKeyType, err.message)
+    } else {
+      errorHandlers.handle('Unhandled-Promise-Rejection')
+    }
+
+    console.error('UnhandledRejection', event)
+    event.preventDefault()
+  }
+}
