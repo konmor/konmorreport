9f9c811b34e7688701aef8707273e09e161d9fde
konmor <5363420+konmor@user.noreply.gitee.com>
2025-06-24 00:18:13 +0800

    feat: 标签模板

--- src/components/chart/MetricsCard.vue
+++ src/components/chart/MetricsCard.vue
@@ -14,12 +14,10 @@ let options = ref({
     // 位置 left right center 均在顶部
     position: 'left',
     textStyle: {
-      color: '#fff',
-      fontSize: 14,
-      // bold bolder lighter normal
+      color: '#e8c917',
+      fontSize: 36,
       fontWeight: 'bold',
-      height: 38,
-
+      height: 60,
     },
   },
   content: {
@@ -28,47 +26,56 @@ let options = ref({
         id: '1',
         padding: [16, 8, 16, 8],
         // 二选其一
-        minWidth: 128,
-        width: 128,
+        minWidth: 486,
+        width: null,
+        minHeight:70,
+        height:null,
         name: {
           text: '',
           show: false,
           itemStyle: {
-            color: '#fff',
+            color: '#000',
             fontSize: 14,
-            // bold bolder lighter normal
             fontWeight: 'bold',
-            width: 32,
-            height: 36,
+            width: null,
+
             minWidth: 32,
-            padding: [0, 4]
+            padding: [0, 4],
+            // start center end
+            alignSelf:'center',
           }
         },
         value: {
+          width:null,
+          minWidth:128,
           prefix: {
             text: '',
             show: false,
             itemStyle: {
-              color: '#fff',
+              color: '#000',
               fontSize: 14,
               // bold bolder lighter normal
               fontWeight: 'bold',
-              height: 36,
-              width: 32,
+
+              width: null,
               minWidth: 32,
+              // start center end
+              alignSelf:'center',
             }
           },
           metrics: {
             text: '3.1',
             show: true,
             itemStyle: {
-              color: '#fff',
-              fontSize: 14,
+              color: '#6ddf34',
+              fontSize: 70,
               // bold bolder lighter normal
-              fontWeight: 'bold',
-              height: 36,
-              width: 32,
+              fontWeight: 'normal',
+              height: 92, // 22
+              width: null,
               minWidth: 32,
+              // start center end
+              alignSelf:'center',
             }
           },
           suffix: {
@@ -76,13 +83,15 @@ let options = ref({
             text: '万台' as string | Component,
             show: true,
             itemStyle: {
-              color: '#fff',
+              color: '#000',
               fontSize: 14,
               // bold bolder lighter normal
               fontWeight: 'bold',
-              height: 36,
-              width: 32,
+
+              width: null,
               minWidth: 32,
+              // start center end
+              alignSelf:'end',
             }
           }
         },
@@ -92,47 +101,57 @@ let options = ref({
         id: '2',
         padding: [16, 8, 16, 8],
         // 二选其一
-        minWidth: 128,
-        width: 128,
+        minWidth: 240,
+        width: null,
+        minHeight:70,
+        height:null,
         name: {
           text: '同比增长',
           show: true,
           itemStyle: {
-            color: '#fff',
+            color: '#000',
             fontSize: 14,
             // bold bolder lighter normal
             fontWeight: 'bold',
-            width: 32,
-            height: 36,
+            width: null,
+
             minWidth: 32,
-            padding: [0, 4]
+            padding: [0, 4],
+            // start center end
+            alignSelf:'end',
           }
         },
         value: {
+          width:null,
+          minWidth:128,
           prefix: {
-            text: '',
-            show: false,
+            text: '同比',
+            show: true,
             itemStyle: {
-              color: '#fff',
+              color: '#000',
               fontSize: 14,
               // bold bolder lighter normal
               fontWeight: 'bold',
-              height: 36,
-              width: 32,
+
+              width: null,
               minWidth: 32,
+              // start center end
+              alignSelf:'start',
             }
           },
           metrics: {
             text: '0.9%',
             show: true,
             itemStyle: {
-              color: '#fff',
+              color: '#000',
               fontSize: 14,
               // bold bolder lighter normal
               fontWeight: 'bold',
-              height: 36,
-              width: 32,
+
+              width: null,
               minWidth: 32,
+              // start center end
+              alignSelf:'center',
             }
           },
           suffix: {
@@ -140,13 +159,15 @@ let options = ref({
             text: TrendUp as string | Component,
             show: true,
             itemStyle: {
-              color: '#fff',
-              fontSize: 14,
+              color: '#67ea44',
+              fontSize: 18,
               // bold bolder lighter normal
-              fontWeight: 'bold',
-              height: 36,
-              width: 32,
+              fontWeight: 'bolder',
+
+              width: null,
               minWidth: 32,
+              // start center end
+              alignSelf:'start',
             }
           }
         },
@@ -156,33 +177,39 @@ let options = ref({
         id: '3',
         padding: [16, 8, 16, 8],
         // 二选其一
-        minWidth: 128,
-        width: 128,
+        minWidth: 240,
+        width: null,
+        minHeight:70,
+        height:null,
         name: {
           text: '美国仓库',
           show: true,
           itemStyle: {
-            color: '#fff',
+            color: '#000',
             fontSize: 14,
             // bold bolder lighter normal
             fontWeight: 'bold',
-            width: 32,
-            height: 36,
+            width: null,
+
             minWidth: 32,
-            padding: [0, 4]
+            padding: [0, 4],
+            // start center end
+            alignSelf:'center',
           }
         },
         value: {
+          width:null,
+          minWidth:128,
           prefix: {
             text: '',
             show: false,
             itemStyle: {
-              color: '#fff',
+              color: '#000',
               fontSize: 14,
               // bold bolder lighter normal
               fontWeight: 'bold',
-              height: 36,
-              width: 32,
+
+              width: null,
               minWidth: 32,
             }
           },
@@ -190,12 +217,12 @@ let options = ref({
             text: '1.8万台',
             show: true,
             itemStyle: {
-              color: '#fff',
+              color: '#000',
               fontSize: 14,
               // bold bolder lighter normal
               fontWeight: 'bold',
-              height: 36,
-              width: 32,
+
+              width: null,
               minWidth: 32,
             }
           },
@@ -204,12 +231,12 @@ let options = ref({
             text: TrendUp as string | Component,
             show: true,
             itemStyle: {
-              color: '#fff',
+              color: '#000',
               fontSize: 14,
               // bold bolder lighter normal
               fontWeight: 'bold',
-              height: 36,
-              width: 32,
+
+              width: null,
               minWidth: 32,
             }
           }
@@ -220,33 +247,39 @@ let options = ref({
         id: '4',
         padding: [16, 8, 16, 8],
         // 二选其一
-        minWidth: 128,
-        width: 128,
+        minWidth: 240,
+        width: null,
+        minHeight:70,
+        height:null,
         name: {
           text: '加拿大库存',
           show: true,
           itemStyle: {
-            color: '#fff',
+            color: '#000',
             fontSize: 14,
             // bold bolder lighter normal
             fontWeight: 'bold',
-            width: 32,
-            height: 36,
+            width: null,
+
             minWidth: 32,
-            padding: [0, 4]
+            padding: [0, 4],
+            // start center end
+            alignSelf:'center',
           }
         },
         value: {
+          width:null,
+          minWidth:128,
           prefix: {
             text: '',
             show: false,
             itemStyle: {
-              color: '#fff',
+              color: '#000',
               fontSize: 14,
               // bold bolder lighter normal
               fontWeight: 'bold',
-              height: 36,
-              width: 32,
+
+              width: null,
               minWidth: 32,
             }
           },
@@ -254,12 +287,12 @@ let options = ref({
             text: '2582台',
             show: true,
             itemStyle: {
-              color: '#fff',
+              color: '#000',
               fontSize: 14,
               // bold bolder lighter normal
               fontWeight: 'bold',
-              height: 36,
-              width: 32,
+
+              width: null,
               minWidth: 32,
             }
           },
@@ -268,12 +301,12 @@ let options = ref({
             text: TrendDown as string | Component,
             show: true,
             itemStyle: {
-              color: '#fff',
+              color: '#000',
               fontSize: 14,
               // bold bolder lighter normal
               fontWeight: 'bold',
-              height: 36,
-              width: 32,
+
+              width: null,
               minWidth: 32,
             }
           }
@@ -284,33 +317,39 @@ let options = ref({
         id: '5',
         padding: [16, 8, 16, 8],
         // 二选其一
-        minWidth: 128,
-        width: 128,
+        minWidth: 240,
+        width: null,
+        minHeight:70,
+        height:null,
         name: {
           text: '墨西哥仓库',
           show: true,
           itemStyle: {
-            color: '#fff',
+            color: '#000',
             fontSize: 14,
             // bold bolder lighter normal
             fontWeight: 'bold',
-            width: 32,
-            height: 36,
+            width: null,
+
             minWidth: 32,
-            padding: [0, 4]
+            padding: [0, 4],
+            // start center end
+            alignSelf:'center',
           }
         },
         value: {
+          width:null,
+          minWidth:128,
           prefix: {
             text: '',
             show: false,
             itemStyle: {
-              color: '#fff',
+              color: '#000',
               fontSize: 14,
               // bold bolder lighter normal
               fontWeight: 'bold',
-              height: 36,
-              width: 32,
+
+              width: null,
               minWidth: 32,
             }
           },
@@ -318,12 +357,12 @@ let options = ref({
             text: '1万台',
             show: true,
             itemStyle: {
-              color: '#fff',
+              color: '#000',
               fontSize: 14,
               // bold bolder lighter normal
               fontWeight: 'bold',
-              height: 36,
-              width: 32,
+
+              width: null,
               minWidth: 32,
             }
           },
@@ -332,12 +371,82 @@ let options = ref({
             text: TrendDown as string | Component,
             show: true,
             itemStyle: {
-              color: '#fff',
+              color: '#000',
               fontSize: 14,
               // bold bolder lighter normal
               fontWeight: 'bold',
-              height: 36,
-              width: 32,
+
+              width: null,
+              minWidth: 32,
+            }
+          }
+        },
+
+      },
+      {
+        id: '6',
+        padding: [16, 8, 16, 8],
+        // 二选其一
+        minWidth: 240,
+        width: null,
+        minHeight:70,
+        height:null,
+        name: {
+          text: '墨西哥仓库',
+          show: true,
+          itemStyle: {
+            color: '#000',
+            fontSize: 14,
+            // bold bolder lighter normal
+            fontWeight: 'bold',
+            width: null,
+
+            minWidth: 32,
+            padding: [0, 4],
+            // start center end
+            alignSelf:'center',
+          }
+        },
+        value: {
+          width:null,
+          minWidth:128,
+          prefix: {
+            text: '',
+            show: false,
+            itemStyle: {
+              color: '#000',
+              fontSize: 14,
+              // bold bolder lighter normal
+              fontWeight: 'bold',
+
+              width: null,
+              minWidth: 32,
+            }
+          },
+          metrics: {
+            text: '1万台',
+            show: true,
+            itemStyle: {
+              color: '#000',
+              fontSize: 14,
+              // bold bolder lighter normal
+              fontWeight: 'bold',
+
+              width: null,
+              minWidth: 32,
+            }
+          },
+          suffix: {
+            isIcon: true,
+            text: TrendDown as string | Component,
+            show: true,
+            itemStyle: {
+              color: '#000',
+              fontSize: 14,
+              // bold bolder lighter normal
+              fontWeight: 'bold',
+
+              width: null,
               minWidth: 32,
             }
           }
@@ -362,29 +471,69 @@ let options = ref({
           color:options.title.textStyle.color,
           justifyContent:options.title.position,
          }"><span>{{ options.title.text }}</span></div>
-    <div class="content">
-      <div class="label">
-        <div class="name">
-          <span></span>
+    <div class="content" v-if="options.content.labels.length>0">
+      <div class="label" v-for="(item,index) in options.content.labels"
+      :id="item.id"
+           :style="
+           {
+             padding:item.padding.join('px ').concat('px'),
+             minWidth:item.minWidth+'px',
+             width:item.width?item.width+'px':null,
+             minHeight:item.minHeight+'px',
+             height:item.height?item.height+'px':null,
+            }">
+        <div class="name" v-if="item.name.show"
+        :style="{
+          fontSize:item.name.itemStyle.fontSize +'px',
+          color:item.name.itemStyle.color,
+          fontWeight:item.name.itemStyle.fontWeight,
+          width:item.name.itemStyle.width?item.name.itemStyle.width+'px':null,
+          minWidth:item.name.itemStyle.minWidth?item.name.itemStyle.minWidth+'px':null,
+          padding:item.name.itemStyle.padding.join('px ').concat('px'),
+          alignSelf:item.name.itemStyle.alignSelf,
+        }">
+          <span>{{item.name.text}}</span>
         </div>
-        <div class="value">
-          <span class="prefix"></span>
-          <span class="metrics" style="font-weight: bolder;font-size: 90px">3.1</span>
-          <span class="suffix">万台</span>
+        <div class="value" :style="{
+                width:item.value.width?item.value.width+'px':null,
+                minWidth:item.value.minWidth?item.value.minWidth+'px':null,
+        }">
+          <span class="prefix" v-if="item.value.prefix.show"
+                :style="{
+            fontSize:item.value.prefix.itemStyle.fontSize +'px',
+            color:item.value.prefix.itemStyle.color,
+            fontWeight:item.value.prefix.itemStyle.fontWeight,
+            width:item.value.prefix.itemStyle.width?item.value.prefix.itemStyle.width+'px':null,
+            minWidth:item.value.prefix.itemStyle.minWidth?item.value.prefix.itemStyle.minWidth+'px':null,
+            alignSelf:item.value.prefix.itemStyle.alignSelf,
+          }">{{item.value.prefix.text}}</span>
+          <span class="metrics" v-if="item.value.metrics.show"
+                :style="{
+            fontSize:item.value.metrics.itemStyle.fontSize+'px',
+            color:item.value.metrics.itemStyle.color,
+            fontWeight:item.value.metrics.itemStyle.fontWeight,
+            width:item.value.metrics.itemStyle.width?item.value.metrics.itemStyle.width+'px':null,
+            height:item.value.metrics.itemStyle.height+'px',
+            lineHeight:item.value.metrics.itemStyle.height+'px',
+            minWidth:item.value.metrics.itemStyle.minWidth?item.value.metrics.itemStyle.minWidth+'px':null,
+            alignSelf:item.value.metrics.itemStyle.alignSelf,
+          }">{{item.value.metrics.text}}</span>
+          <span class="suffix" v-if="item.value.suffix.show"
+                :style="{
+            fontSize:item.value.suffix.itemStyle.fontSize +'px',
+            color:item.value.suffix.itemStyle.color,
+            fontWeight:item.value.suffix.itemStyle.fontWeight,
+            width:item.value.suffix.itemStyle.width?item.value.suffix.itemStyle.width+'px':null,
+            minWidth:item.value.suffix.itemStyle.minWidth?item.value.suffix.itemStyle.minWidth+'px':null,
+            alignSelf:item.value.suffix.itemStyle.alignSelf,
+          }">
+           <span v-if="item.value.suffix.isIcon">
+             <component  :is="item.value.suffix.text"></component>
+           </span>
+            <span v-else>{{item.value.suffix.text}} </span>
+            </span>
         </div>
       </div>
-
-      <div class="label">
-        <div class="name">
-          <span>同比增长</span>
-        </div>
-        <div class="value">
-          <span class="prefix"></span>
-          <span class="metrics">0.9%</span>
-          <span class="suffix"><TrendUp/></span>
-        </div>
-      </div>
-
     </div>
   </div>
 </template>
@@ -395,9 +544,6 @@ let options = ref({
   width: 100%;
   height: 100%;
   padding: 8px;
-  border-radius: 12px;
-  border: 1px solid black;
-
 }
 
 .chart-label > .title {
@@ -421,76 +567,41 @@ let options = ref({
 }
 
 .chart-label .content .label {
-  padding: 16px 8px 16px 8px;
-
   display: flex;
-  align-items: center;
+
+  /*align-items: center;*/
   justify-content: space-between;
-  min-width: 128px;
-  border: 1px solid #6fd845;
+  /*卡片效果*/
+  margin-right: 6px;
+  border-radius: 8px;
+  box-shadow: 0 4px 8px 0 rgba(0,0,0,0.2);
 }
 
+.chart-label .content .label:hover {
+  cursor: pointer;
+  transition: box-shadow 0.2s, border-color 0.2s;
+  box-shadow: 0 8px 16px 0 rgba(0,0,0,0.2);
+}
+
+
 .chart-label .content .label > .name {
+  display: block;
   height: 36px;
   line-height: 36px;
-  /* 1. 字体大小 */
-  font-size: 14px;
-  /* 2. 字体宽度*/
-  font-weight: normal;
-  /* 3. 字体颜色*/
-  color: red;
-  /*background-color: #efb056;*/
-
-  display: block;
-
-  padding: 0 4px;
-  min-width: 32px;
-  /* border: 1px solid #6fd845;*/
 }
 
 .chart-label .content .label > .value {
   display: flex;
   justify-content: space-between;
-  align-items: center;
+  /*align-items: center;*/
   padding: 0 4px 0 0;
 }
-
-.chart-label .content .label > .value .prefix {
+.chart-label .content .label > .value .prefix ,
+.chart-label .content .label > .value .metrics,
+.chart-label .content .label > .value .suffix{
   height: 36px;
   line-height: 36px;
-
-  /*字体大小 粗细 颜色*/
-  font-size: 16px;
-  font-weight: normal;
-  color: red;
-
-  /*background-color: #efb056;*/
-
-  /*border: 1px solid #6fd845;*/
-}
-
-.chart-label .content .label > .value .metrics {
-  height: 36px;
-  line-height: 36px;
-  /*字体大小 粗细 颜色*/
-  font-size: 16px;
-  font-weight: normal;
-  color: red;
-  /*background-color: #efb056;*/
-
-  /*border: 1px solid #6fd845;*/
 }
 
 
-.chart-label .content .label > .value .suffix {
-  height: 36px;
-  line-height: 36px;
-  /*字体大小 粗细 颜色*/
-  font-size: 16px;
-  font-weight: normal;
-  color: red;
-  /*background-color: #efb056;*/
-
-  /* border: 1px solid #6fd845;*/
-}
 </style>
