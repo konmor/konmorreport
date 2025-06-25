d8b13731e1d4a62ed75797dc95c62e2ade87959b
konmor <5363420+konmor@user.noreply.gitee.com>
2025-06-26 01:39:09 +0800

    feat: 表格

--- src/components/chart/table/Table.vue
+++ src/components/chart/table/Table.vue
@@ -1,165 +1,87 @@
 <script setup lang="ts">
-import {ref} from "vue";
-import {usePagination} from "vue-request";
-import {sqlQueryData} from "@/api/sql.ts";
-import type {PageInfo, SQLParam, SQLQuery} from "@/types/api.ts";
+import { computed, ref, watch } from 'vue'
+import { usePagination } from 'vue-request'
+import { sqlQueryData } from '@/api/sql.ts'
+import type { TableProps } from 'ant-design-vue'
 
-let table = ref({
-  datas: [
-    {
-      key: '1',
-      name: '胡彦斌',
-      age: 32,
-      address: '西湖区湖底公园1号',
-    },
-    {
-      key: '2',
-      name: '胡彦祖',
-      age: 42,
-      address: '西湖区湖底公园1号',
-    },
-    {
-      key: '3',
-      name: '张三水',
-      age: 35,
-      address: '中国、杭州、西湖区湖底公园2号',
-    },
-    {
-      key: '4',
-      name: '也老大',
-      age: 29,
-      address: '中国、四川、成都市西湖街凤溪居13号',
-    },
-    {
-      key: '5',
-      name: '而老大',
-      age: 45,
-      address: '中国、四川、成都市西湖街凤溪居13号',
-    },
-    {
-      key: '6',
-      name: '胡彦斌1',
-      age: 32,
-      address: '西湖区湖底公园1号',
-    },
-    {
-      key: '7',
-      name: '胡彦祖2',
-      age: 42,
-      address: '西湖区湖底公园1号',
-    },
-    {
-      key: '8',
-      name: '张三水3',
-      age: 35,
-      address: '中国、杭州、西湖区湖底公园2号',
-    },
-    {
-      key: '9',
-      name: '也老大4',
-      age: 29,
-      address: '中国、四川、成都市西湖街凤溪居13号',
-    },
-    {
-      key: '10',
-      name: '而老大5',
-      age: 45,
-      address: '中国、四川、成都市西湖街凤溪居13号',
-    },
-  ],
-  columns: [
-    {
-      title: '姓名',
-      dataIndex: 'name',
-      key: 'name',
-    },
-    {
-      title: '年龄',
-      dataIndex: 'age',
-      key: 'age',
-    },
-    {
-      title: '住址',
-      dataIndex: 'address',
-      key: 'address',
-    },
-  ],
+const { data, current, pageSize, loading, total, refresh, run } = usePagination(sqlQueryData, {
   pagination: {
-    total: 10,
-    current: 1,
-    pageSize: 5,
+    currentKey: 'pageInfo.page',
+    pageSizeKey: 'pageInfo.size',
+    totalKey: 'data.total',
   },
-  size: "small",
-  style: {
-    overflowX: 'auto',
-    overflowY: 'auto',
+  defaultParams: [
+    {
+      sourceId: '10001',
+      sqlId: 3,
+      queryBySQLContent: false,
+      pageInfo: { page: 1, size: 10, total: 0 },
+    },
+  ],
+})
+
+const columns = ref<Array<{ title: string; key: string | number; dataIndex: string | number }>>([])
+
+const pagination = computed(() => ({
+  total: total.value,
+  current: current.value,
+  pageSize: pageSize.value,
+  showSizeChanger:true,
+  showQuickJumper:true,
+}))
+
+
+
+watch(data, () => {
+  if (data.value.data.columns != null && data.value.data.columns.length > 0) {
+    columns.value = data.value.data.columns.map((item) => {
+      return { title: item.fieldAlias, dataIndex: item.fieldAlias, key: item.fieldId }
+    })
   }
 })
 
-// {data, total, loading, page, pageSize}
-
-const {
-  data,
-  current,
-  pageSize,
-  loading,
-  total,
-  refresh
-} = usePagination(
-    (pg) => {
-      let sqlQuery: SQLQuery = {
-        sourceId: 10001,
-        sqlId: 13,
-        sqlContent: '',
-        queryBySQLContent: false,
-        sqlParamList: [
-          {
-            paramId: '',
-            sqlId: '',
-            paramName: '',
-            defaultValue: '',
-          }
-        ],
-        pageInfo: {
-          page: pg.current,
-          size: pg.pageSize,
-          total: 0
-        },
-      }
-
-      return sqlQueryData(sqlQuery);
-    }, {
-      append: false,  // 数据不追加
-      data: res => res.data.data,  // 定义如何取data数据
-      total: res => res.data.total,    // 定义如何取total数据
-      defaultPageSize: 10,
-      defaultCurrent: 1,
-    }
-);
-
-console.log('result',
-    data,
-    current,
-    pageSize,
-    loading,
-    total,
-    refresh);
-
+const handleTableChange: TableProps['onChange'] = (pag: { pageSize: number; current: number }) => {
+  run({
+    sourceId: 10001,
+    sqlId: 3,
+    queryBySQLContent: false,
+    pageInfo: {
+      page: pag.current,
+      size: pag.pageSize,
+      total: 0,
+    },
+  })
+}
 </script>
 
 <template>
   <!--      :scroll="{ x: scrollX, y: scrollY }"
       :loading="loading"-->
-  <a-table
-      class="ant-table-striped"
-      :data-source="table.datas"
-      :columns="table.columns"
-      :pagination="table.pagination"
-      :size="table.size"
-      :rowClassName="(_record: any, index: number) => (index % 2 === 1 ? 'table-striped' : null)"
-      bordered
-      :style="table.style">
+  <!--  <a-table
+        class="ant-table-striped"
+        :data-source="table.datas"
+        :columns="table.columns"
+        :pagination="table.pagination"
+        :size="table.size"
+        :rowClassName="(_record: any, index: number) => (index % 2 === 1 ? 'table-striped' : null)"
+        bordered
+        :style="table.style">
+    </a-table>-->
 
+  <a-table
+    class="ant-table-striped"
+    :rowClassName="(_record: any, index: number) => (index % 2 === 1 ? 'table-striped' : null)"
+    :columns="columns"
+    :data-source="data?.data.data"
+    :pagination="pagination"
+    :loading="loading"
+    bordered
+    size="small"
+    @change="handleTableChange"
+  >
+    <template #bodyCell="{ column, text }">
+      <template v-if="column.dataIndex === 'name'">{{ text.first }} {{ text.last }}</template>
+    </template>
   </a-table>
 </template>
 
